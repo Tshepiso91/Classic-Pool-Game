6da79424bab4f6b8e269dbf9944fa9c403e97f8b
Tshepiso91 <tr.masekoameng@gmail.com>
2022-12-29 18:24:53 +0200

    Added functionality to stick, implemented collision of stick to the ball, and reset co-ordinates of the stick

--- ball.js
+++ ball.js
@@ -2,12 +2,18 @@ const BALL_ORIGIN = new Vector2(25, 25);
 
 function Ball(position) {
     this.position = position;
+    this.velocity = new Vector2()
 }
 
-Ball.prototype.update = function() {
-
+Ball.prototype.update = function(delta) {
+    this.position.addTo(this.velocity.mult(delta));
+    this.velocity = this.velocity.mult(0.98)
 }
 
 Ball.prototype.draw = function() {
     Canvas.drawImage(sprites.yellowBall, this.position, BALL_ORIGIN)
+}
+
+Ball.prototype.shoot = function(power, rotation) {
+    this.velocity = new Vector2(power * Math.cos(rotation), power* Math.sin(rotation))
 }
--- gameWorld.js
+++ gameWorld.js
@@ -1,15 +1,20 @@
 // Containing all the physical objects(ball, sticks, table and sounds) of the game
 // Responsible for updating and Drawing them into the game
+const DELTA = 1/100
+
 function GameWorld() {
 
     this.yellowBall = new Ball(new Vector2(413, 413))
-    this.stick = new Stick()
+    this.stick = new Stick(
+        new Vector2(413, 413), 
+        this.yellowBall.shoot
+        .bind(this.yellowBall))
 }
 
 GameWorld.prototype.update = function() {
 
     this.stick.update()
-    this.yellowBall.update()
+    this.yellowBall.update(DELTA)
 }
 
 GameWorld.prototype.draw = function() {
